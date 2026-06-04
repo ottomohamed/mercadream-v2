200a459a5c0cb686b72b6d09a895ed7efaeadebb
ottomohamed <miosegundamano9@gmail.com>
2026-06-04 17:07:37 +0200

    FIX: wavespeed size 480*832

--- api/index.js
+++ api/index.js
@@ -509,3 +509,4 @@ module.exports = async function handler(req, res) {
   try { await fn(req, res); }
   catch(e) { console.error('['+service+']', e.message); return res.status(500).json({ error:e.message }); }
 };
+
